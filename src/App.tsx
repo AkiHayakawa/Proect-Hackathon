681622efea511495bb5b6bdbb4a6018ca8640f9f
user <akbarabdurahmanov864@gmail.com>
2023-07-28 18:03:15 +0600

    navigate in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Routes, useNavigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 import MainRoutes from "./routes";
 import Navbar from "./components/auth/Navbar";
 
 function App() {
+  const navigate = useNavigate();
+  const registrId = localStorage.getItem("registr-id");
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!registrId && location.pathname == "/") {
+      navigate("/registr");
+    }
+  }, []);
   return (
     <BrowserRouter>
       <Navbar />
--- src/components/auth/Navbar.tsx
+++ src/components/auth/Navbar.tsx
@@ -2,15 +2,6 @@ import React, { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  const navigate = useNavigate();
-  const registrId = localStorage.getItem("registr-id");
-  const location = useLocation();
-
-  useEffect(() => {
-    if (!registrId && location.pathname == "/") {
-      navigate("/registr");
-    }
-  }, []);
   return <div className="navbar-auth">navbar</div>;
 };
 
