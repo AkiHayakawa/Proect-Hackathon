7b04ac89dcffd40151bdc648e23168077b573259
user <akbarabdurahmanov864@gmail.com>
2023-07-28 18:27:39 +0600

    navigate in routes

--- src/App.tsx
+++ src/App.tsx
@@ -11,15 +11,6 @@ import MainRoutes from "./routes";
 import Navbar from "./components/auth/Navbar";
 
 function App() {
-  const navigate = useNavigate();
-  const registrId = localStorage.getItem("registr-id");
-  const location = useLocation();
-
-  useEffect(() => {
-    if (!registrId && location.pathname == "/") {
-      navigate("/registr");
-    }
-  }, []);
   return (
     <BrowserRouter>
       <Navbar />
--- src/components/auth/Navbar.tsx
+++ src/components/auth/Navbar.tsx
@@ -2,6 +2,15 @@ import React, { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const registrId = localStorage.getItem("registr-id");
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!registrId && location.pathname == "/") {
+      navigate("/registr");
+    }
+  }, []);
   return <div className="navbar-auth">navbar</div>;
 };
 
